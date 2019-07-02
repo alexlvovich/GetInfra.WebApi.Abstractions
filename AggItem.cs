b335806784fe01bfbbd3852ee1c55ab79cf68556
Alexander Lvovich <alex.lvovich@indevlabs.de>
2019-07-02 13:49:41 +0200

    aggregations with response

---
+++ AggItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetInfra.WebApi.Abstractions
+{
+    public class AggItem
+    {
+        public string Name { get; set; }
+        public long Count { get; set; }
+
+        public string Id { get; set; }
+    }
+}
---
+++ PagedResponseWithAggs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetInfra.WebApi.Abstractions
+{
+    public class PagedResponseWithAggs<T> : PagedResponse<T>
+    {
+        public PagedResponseWithAggs() { }
+
+        public PagedResponseWithAggs(PagedResponse<T> res)
+        {
+            this.List = res.List;
+            this.Total = res.Total;
+        }
+
+        public Dictionary<string, List<AggItem>> Aggs { get; set; }
+    }
+}
